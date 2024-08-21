d78794f1ea09782242d5cfcdc0447c88976ba2b6
Andres Felipe Rico <andrespro2004@gmail.com>
2024-08-20 19:02:23 -0500

    firts commit

---
+++ src/test/java/com/rico/andres/AndresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rico.andres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
